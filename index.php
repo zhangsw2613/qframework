511938ce7e7ddb9ddcfed762e0c86407bba5ea71
zhangsw2613 <zhangsw2613@163.com>
2018-08-22 13:56:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * @since   Version 1.0.0
  */
 
-if (version_compare(PHP_VERSION, '5.5.0', '<')) die('require PHP > 5.5.0 !');
+if (version_compare(PHP_VERSION, '7.0.0', '<')) die('require PHP >= 7.0.0 !');
 // 应用程序目录
 define('APP_PATH', __DIR__ . '/demo');
 // 引入QFramework核心文件
